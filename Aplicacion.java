b7a3070d76344628ea0ba897504bb3b182115392
hectorsanchez-eng <160237425+hectorsanchez-eng@users.noreply.github.com>
2024-03-08 12:06:40 +0100

    Update Aplicacion.java

--- Aplicacion.java
+++ Aplicacion.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int numero1 = 20;
+        int numero1 = 10;
         int numero2 = 50;
         int resultado = numero1 + numero2;
         System.out.print(resultado);
